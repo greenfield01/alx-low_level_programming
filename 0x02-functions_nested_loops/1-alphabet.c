9a420ef5e1d9811b9cb939b20b3590c845ee2ced
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-13 14:09:56 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: code to write out all alphabets and new line
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar(\n);
+}
