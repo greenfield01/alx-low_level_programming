2db777d3be690772596c1135c7f3477c8386564d
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-13 15:47:09 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/* _islower - entry point
+ *
+ * Description: check for lower case characters
+ *
+ * Return: int
+ */
+
+int _islower(int c)
+{
+	for (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return(9)
+}
+		
+
