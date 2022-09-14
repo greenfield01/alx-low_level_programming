58a943591f918a7078cbb031ecc49b766bec3cac
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-13 18:46:03 -0700

    jksldjf

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * print_sign - entry point
+ * @n: 
+ * Return: 9 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{	_putchar(48)
+		return (-1)
+	}
+	else
+	{
+		_putchar(45);
+		return (0)
+	}
