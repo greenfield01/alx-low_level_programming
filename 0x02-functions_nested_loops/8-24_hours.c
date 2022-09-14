957b3b6884057dee956ff9649e940453c1d34d03
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-14 16:53:36 -0700

    no 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,7 +21,8 @@ void jack_bauer(void)
 			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');							_putchar('\n');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 			m++;
 		}
 		h++;
