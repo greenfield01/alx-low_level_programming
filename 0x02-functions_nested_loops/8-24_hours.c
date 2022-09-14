8fe453bc1b215e8a6ebd0b81fac079549857ac65
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-14 15:29:27 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * jack_bauer - print void of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 0;
+
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+
+
