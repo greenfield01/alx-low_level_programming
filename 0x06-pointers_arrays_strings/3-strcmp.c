791cdb4c7a9a2a8def83697440a2e83cd718e8bf
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-21 16:41:23 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,4 @@
-#include <string.h>
-#include <stdio.h>
-#include "main.h"
-
+#include "main.h" 
 /**
  * _strcmp - compare two strings
  * @s1: string 1
@@ -11,10 +8,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int x;
+
 	s1 = "lastname";
 	s2 = "firstname";
+	
+	x = _strcmp(s1, s2);
+/*	printf("strcmp(s1, s2) = %d\n", strcmp(s1, s2)); */
 
-	printf("strcmp(s1, s2) = %d\n", strcmp(s1, s2));
-
-	return 0;
+	return (x);
 }
+
