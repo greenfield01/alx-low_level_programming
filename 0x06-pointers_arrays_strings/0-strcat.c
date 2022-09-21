41641c0f90cf829b4597f3ea7c822e6d8fae9399
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-21 13:47:17 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: first and return string
+ * @src: concatenated string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
