f26afb164136e2dcb0022fa039c57c6286a95d56
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-09-21 15:06:07 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: return 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	result = strcmp(s1, s2);
+
+	return 0;
+}
